3d0054ced95dbe421785425e9d4af19a5caf130e
Posew7 <posew7@gmail.com>
2019-05-23 20:14:04 +0300

    php mysql

---
+++ bolum_2/array_2.php
@@ -0,0 +1,58 @@
+<?php
+
+/*  print_r()   diziyi yazdırır
+$arr = [
+    "ad" => "yunus",
+    "soyad" => "üstün",
+    "yas" => 23,
+    "şehir" => "antalya"
+];
+
+print_r($arr);
+ */
+
+/*  explode()   stringi ayırarak dizi yapar
+$test = "yunus,üstün,23,antalya";
+$arr = explode(",",$test);
+print_r($arr);
+ */
+
+/*  implode()   diziye string yapar
+$arr = ["yunus","üstün","23","antalya"];
+$test = implode("_",$arr);
+print $test;
+ */
+
+/*  count()     dizide eleman sayısı
+$arr = [1,2,3,4,5,6,7];
+print count($arr);
+ */
+
+/*  is_array()  dizi olup olmadığı
+$arr = [1,2,3,4,5,6,7];
+is_array($arr);
+ */
+
+/*
+$arr = [1,2,3,4,5,6,7];
+shuffle($arr);
+print_r($arr);
+ */
+
+/*
+$arr1 = ["ad","yaş","şehir"];
+$arr2 = ["yunus",23,"antalya"];
+$arr3 = array_combine($arr1, $arr2);
+print_r($arr3);
+ */
+
+/*
+$arr = ["yunus",23,23,"yunus",23,"antalya"];
+$arr2 = array_count_values($arr);
+print_r($arr2);
+ */
+
+/*
+
+ */
+
